d69d51e82b0b627fdd7ab02b3ec2e8d2a9d00928
Alice Gimenez Dutra <alicegzdutra@gmail.com>
2024-09-04 13:17:31 -0300

    menu

---
+++ Lista2/cabecalho.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Programação Web</title>
+    <style>
+      .btn-dark:hover {
+        background-color:	black;
+      }
+      .navbar-brand {
+        font-size: 2rem;
+      }
+    </style>
+  </head>
+
+  <body class="bg-dark text-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid justify-content-center">
+        <a class="navbar-brand">Atividades de Programação Web</a>
+      </div>
+    </nav>
+    
---
+++ Lista2/menu.php
@@ -0,0 +1,11 @@
+<?php require("cabecalho.php"); ?>
+
+    <div class="container mt-4">  
+      <div class="d-flex flex-column align-items-center">
+        <button onclick="window.location='exer1.php'" type="button" class="btn btn-dark btn-lg mb-3 border border-light text-center" style="width: 50%;">Exercício 1</button>
+        <button onclick="window.location='exer2.php'" type="button" class="btn btn-dark btn-lg mb-3 border border-light text-center" style="width: 50%;">Exercício 2</button>
+        <button onclick="window.location='exer3.php'" type="button" class="btn btn-dark btn-lg mb-3 border border-light text-center" style="width: 50%;">Exercício 3</button>
+    </div>
+    </div>
+
+<?php require("rodape.php");?>
